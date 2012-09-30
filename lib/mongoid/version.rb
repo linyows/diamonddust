e406a52d84be50771dc175e994bf9676c917b115
linyows <linyows@gmail.com>
2012-09-30 14:14:23 +0000

    Revert "miss"
    This reverts commit 5a420171c6f7a3124ccd05e79d98bea343124dd0.

--- lib/mongoid/version.rb
+++ lib/mongoid/version.rb
@@ -1,3 +1,3 @@
 module MongoidTagging
-  VERSION = '0.0.3'
+  VERSION = '0.0.2'
 end
--- lib/mongoid_tagging.rb
+++ lib/mongoid_tagging.rb
@@ -1,1 +1,1 @@
-require File.join(File.dirname(__FILE__), 'mongoid_tagging')
+require File.join(File.dirname(__FILE__), 'mongoid/tagging')
