0266caa4e4ec5cfb7c90e1502938325ab83e81d5
linyows <linyows@gmail.com>
2012-11-08 13:15:03 +0000

    woops

--- lib/diamonddust/tagging.rb
+++ lib/diamonddust/tagging.rb
@@ -1,44 +1,46 @@
 module Diamonddust
-  extend ActiveSupport::Concern
+  module Tagging
+    extend ActiveSupport::Concern
 
-  included do
-    extend ClassMethods
-    include InstanceMethods
+    included do
+      extend ClassMethods
+      include InstanceMethods
 
-    field :tags, type: Array
-    attr_accessible :tags
-    before_validation :tags_to_array
+      field :tags, type: Array
+      attr_accessible :tags
+      before_validation :tags_to_array
 
-    scope :tagged_with, ->(*tags) {
-      tags = [tags] if tags.is_a?(String)
-      any_in(tags: tags.flatten)
-    }
-  end
+      scope :tagged_with, ->(*tags) {
+        tags = [tags] if tags.is_a?(String)
+        any_in(tags: tags.flatten)
+      }
+    end
 
-  module ClassMethods
-    def all_tags
-      t = self.where(:tags.ne => []).only(:tags).each_with_object [] do |s, result|
-        result.concat(s.tags_origin)
+    module ClassMethods
+      def all_tags
+        t = self.where(:tags.ne => []).only(:tags).each_with_object [] do |s, result|
+          result.concat(s.tags_origin)
+        end
+        t.uniq
       end
-      t.uniq
-    end
-  end
-
-  module InstanceMethods
-    def tags
-      return super unless super.is_a?(Array)
-      (super || []).join(', ')
     end
 
-    def tags_origin
-      self.tags.strip.gsub(/,\s*,/, ',').split(/\s*,\s*/).uniq
-    end
+    module InstanceMethods
+      def tags
+        return super unless super.is_a?(Array)
+        (super || []).join(', ')
+      end
 
-    private
+      def tags_origin
+        self.tags.strip.gsub(/,\s*,/, ',').split(/\s*,\s*/).uniq
+      end
 
-    def tags_to_array
-      return unless self.tags || self.tags.is_a?(String)
-      self.tags = tags_origin
+      private
+
+      def tags_to_array
+        return unless self.tags || self.tags.is_a?(String)
+        self.tags = tags_origin
+      end
     end
   end
 end
